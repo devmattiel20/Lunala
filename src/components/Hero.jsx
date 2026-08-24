008f7b23d9c72720ab7f9bf8148babd68e5e0a96
Mattiel <devmattiel@gmail.com>
2026-08-24 13:12:21 -0500

    Cree la primera version del navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,30 @@
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <p className="hero-label">THE FUTURE OF MUSIC</p>
+
+        <h1>
+          GAMES
+          <span>WITHOUT LIMITS</span>
+        </h1>
+
+        <p className="hero-description">
+          Your Game. Your world.
+        </p>
+
+        <button className="hero-button">
+          START PLAYING
+        </button>
+      </div>
+
+      <div className="hero-visual">
+        <div className="hero-circle">
+          
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav>
+      <h1>Lunala</h1>
+    </nav>
+  )
+}
+
+export default Navbar
